7a8402961cfc1d445622f706da679059b5da0cb9
Don Norum <don@donnorum.com>
2015-12-01 08:24:31 -0500

    Refactors for more verbosity.

--- cluster_shelving.sh
+++ cluster_shelving.sh
@@ -7,9 +7,16 @@ database="cluster_analysis"
 # Set up an empty database with the name above.
 sudo -u $USER dropdb --if-exists $database
 sudo -u $USER createdb $database
+echo "$database dropped and (re)created."
 
 # Load the LibraryThing dump file (converted to CSV) into the database.
-psql $database -f create_and_load_table.sql
+psql $database -f create_and_load_table.sql > /dev/null 2>&1
+echo "LibraryThing dump file loaded into $database."
 
 # Clean up the dimensional fields into standard format and Imperial units.
-psql $database -f clean_and_convert_dimensions.sql
+psql $database -f clean_and_convert_height.sql > /dev/null 2>&1
+echo Height converted into numeric, Imperial values.
+psql $database -f clean_and_convert_length.sql > /dev/null 2>&1
+echo "Length (depth) converted into numeric, Imperial values."
+psql $database -f clean_and_convert_thickness.sql > /dev/null 2>&1
+echo Thickness converted into numeric, Imperial values.
