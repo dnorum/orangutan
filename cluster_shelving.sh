380f1ecc7a04bae33e506d3cf88417a19a8e2694
Don Norum <don@donnorum.com>
2015-12-01 09:59:00 -0500

    Adds comments.

--- cluster_shelving.sh
+++ cluster_shelving.sh
@@ -7,15 +7,24 @@ database="cluster_analysis"
 # Set up an empty database with the name above.
 sudo -u $USER dropdb --if-exists $database
 sudo -u $USER createdb $database
+
+# Status update.
 echo "$database dropped and (re)created."
 
 # Create the table in the database to hold the LibraryThing dump file's data.
 psql $database -f create_table.sql > /dev/null 2>&1
-echo Table created in $database.
 
-# Load the LibraryThing dump file (converted to CSV) into the database.
+# Status update.
+echo "Table created in $database."
+
+# Load the LibraryThing dump file (converted to CSV) into the database and
+# record the number of rows imported.
 n_records_loaded=$(psql $database -f load_table.sql)
+
+# Scrub the output of the postgres command to just the number of rows loaded.
 n_records_loaded=${n_records_loaded//[a-zA-Z ]/}
+
+# Status update.
 echo "${n_records_loaded} records from LibraryThing dump file loaded into $database."
 
 # Clean up the dimensional fields into standard format and Imperial units.
