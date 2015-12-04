f78498a578c52cda08331c041613cf872f9eb58d
Don Norum <don@donnorum.com>
2015-12-04 09:32:46 -0500

    Moves files as part of refactoring.

--- load_table.sql
+++
@@ -1,7 +0,0 @@
--- Load the LibraryThing CSV dump into the table created above. Note that it's
--- currently given as an absolute path.
-COPY library FROM '/home/don/projects/git/cluster_shelving/resources/librarything_dnorum.csv' 
-	DELIMITER ','
-	NULL ''
-	CSV
-	HEADER;
