5b57f1d0553e3381959e34fb038be5c65528a8a8
Don Norum <don@donnorum.com>
2016-01-16 07:55:22 -0500

    Reorders SQL queries to return number of books instead of number of histogram bins.

--- subscripts/sql/output_dimensions.sql
+++ subscripts/sql/output_dimensions.sql
@@ -1,25 +1,5 @@
 COPY
 	(	SELECT
-			book_id AS "Book ID"
-		,	height_scrubbed AS "Height (in)"
-		,	length_scrubbed AS "Width (in)"
-		,	thickness_scrubbed AS "Thickness (in)"
-		,	weight_scrubbed AS "Weight (lb)"
-		FROM
-			library
-		WHERE
-			height_scrubbed IS NOT NULL
-		AND	length_scrubbed IS NOT NULL
-		AND	thickness_scrubbed IS NOT NULL	)
-TO
-	'${DIR}/working/book_dimensions.csv'
-(	FORMAT csv
-,	DELIMITER ','
-,	HEADER
-,	NULL ''	);
-
-COPY
-	(	SELECT
 			((${max_height}-${min_height})/${n_intervals}) * floor(height_scrubbed/((${max_height}-${min_height})/${n_intervals})) + ((${max_height}-${min_height})/${n_intervals})/2.0  AS "Height Bin"
 		,	((${max_length}-${min_length})/${n_intervals}) * floor(length_scrubbed/((${max_length}-${min_length})/${n_intervals})) + ((${max_length}-${min_length})/${n_intervals})/2.0  AS "Length (Width) Bin"
 		,	COUNT(*) AS "Number of Books"
@@ -35,6 +15,26 @@ COPY
 			1 ASC, 2 ASC)
 TO
 	'${DIR}/working/book_heights_lengths.csv'
+(	FORMAT csv
+,	DELIMITER ','
+,	HEADER
+,	NULL ''	);
+
+COPY
+	(	SELECT
+			book_id AS "Book ID"
+		,	height_scrubbed AS "Height (in)"
+		,	length_scrubbed AS "Width (in)"
+		,	thickness_scrubbed AS "Thickness (in)"
+		,	weight_scrubbed AS "Weight (lb)"
+		FROM
+			library
+		WHERE
+			height_scrubbed IS NOT NULL
+		AND	length_scrubbed IS NOT NULL
+		AND	thickness_scrubbed IS NOT NULL	)
+TO
+	'${DIR}/working/book_dimensions.csv'
 (	FORMAT csv
 ,	DELIMITER ','
 ,	HEADER
