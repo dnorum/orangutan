f3b54ec4646d108cb30fd57c81487718df75bc11
Don Norum <don@donnorum.com>
2016-02-05 14:35:45 -0500

    Removes automatic scrubbing of out-of-bounds dimensions.

--- sql/clean_and_convert_weight.sql
+++ sql/clean_and_convert_weight.sql
@@ -7,7 +7,3 @@ ADD COLUMN weight_scrubbed DOUBLE PRECISION;
 UPDATE library
 SET weight_scrubbed = 
 	substring(weight from '^([.0-9]+) (?:pounds?)?$')::DOUBLE PRECISION;
-
-UPDATE library
-SET weight_scrubbed = NULL
-WHERE weight_scrubbed NOT BETWEEN ${weight_min} AND ${weight_max};
