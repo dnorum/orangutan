1b6416fd1b7e1bfb90b98e845fa017d146a80a70
dnorum <don@donnorum.com>
2016-02-05 14:18:40 -0500

    Nabs one last file with length.

--- sql/clean_and_convert_width.sql
+++ sql/clean_and_convert_width.sql
@@ -1,23 +1,23 @@
--- Add a DOUBLE PRECISION field to hold the scrubbed length.
+-- Add a DOUBLE PRECISION field to hold the scrubbed width.
 ALTER TABLE library
-ADD COLUMN length_scrubbed DOUBLE PRECISION;
+ADD COLUMN width_scrubbed DOUBLE PRECISION;
 
--- Scrub the length and convert it into a DOUBLE PRECISION value for the above
+-- Scrub the width and convert it into a DOUBLE PRECISION value for the above
 -- column. Centimeters are converted to inches, while numbers without units are
 -- assumed to be Imperial (inches).
 UPDATE library
-SET length_scrubbed =
-	CASE	WHEN	"length" ~ '^[0-9.]+ inch(es)?$'
-		THEN	substring("length"
+SET width_scrubbed =
+	CASE	WHEN	width ~ '^[0-9.]+ inch(es)?$'
+		THEN	substring(width
 			from '^([0-9.]+) inch(?:es)?$')::DOUBLE PRECISION
-		WHEN	"length" ~ '^[0-9.]+ cm$'
-		THEN	substring("length"
+		WHEN	width ~ '^[0-9.]+ cm$'
+		THEN	substring(width
 			from '^([0-9.]+) cm$')::DOUBLE PRECISION / 2.54
-		WHEN	"length" ~ '^[0-9.]+$'
-		THEN	"length"::DOUBLE PRECISION
+		WHEN	width ~ '^[0-9.]+$'
+		THEN	width::DOUBLE PRECISION
 		ELSE	NULL::DOUBLE PRECISION
 		END;
 
 UPDATE library
-SET length_scrubbed = NULL
-WHERE length_scrubbed NOT BETWEEN ${length_min} AND ${length_max};
+SET width_scrubbed = NULL
+WHERE width_scrubbed NOT BETWEEN ${width_min} AND ${width_max};
