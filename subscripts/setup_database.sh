f2f8f86b86f45b4ef33c7208d261e94b84cec2c5
Don Norum <don@donnorum.com>
2016-02-04 22:05:32 -0500

    Fixes first sed with proper delimiters for file paths in replacement expression.

--- subscripts/setup_database.sh
+++ subscripts/setup_database.sh
@@ -20,7 +20,7 @@ echo "public.library created in $database."
 
 # Load the LibraryThing dump file into the database and record the number of
 # rows imported.
-n_records_loaded=$(psql $database -c "$(sed -e "s/\${DIR}/${DIR}/g" ${DIR}/sql/load_library.sql)")
+n_records_loaded=$(psql $database -c "$(sed -e "s@\${DIR}@${DIR}@g" ${DIR}/sql/load_library.sql)")
 
 # Scrub the output of the postgres command to just the number of rows loaded.
 n_records_loaded=${n_records_loaded//[a-zA-Z ]/}
@@ -37,7 +37,7 @@ do
 	# this is a hideously inefficient invocation inside a loop, but
 	# otherwise it's a nested eval echo construct that I've not yet wrapped
 	# my head around.
-	psql $database -c "$(sed -e "s/\${height_min}/${height_min}/g" -e "s/\${height_max}/${height_max}/g" -e "s/\${length_min}/${length_min}/g" -e "s/\${length_max}/${length_max}/g" -e "s/\${thickness_min}/${thickness_min}/g" -e "s/\${thickness_max}/${thickness_max}/g" -e "s/\${weight_min}/${weight_min}/g" -e "s/\${weight_max}/${weight_max}/g" ${DIR}/subscripts/sql/clean_and_convert_${dimension}.sql)" > /dev/null 2>&1
+	psql $database -c "$(sed -e "s/\${height_min}/${height_min}/g" -e "s/\${height_max}/${height_max}/g" -e "s/\${length_min}/${length_min}/g" -e "s/\${length_max}/${length_max}/g" -e "s/\${thickness_min}/${thickness_min}/g" -e "s/\${thickness_max}/${thickness_max}/g" -e "s/\${weight_min}/${weight_min}/g" -e "s/\${weight_max}/${weight_max}/g" ${DIR}/sql/clean_and_convert_${dimension}.sql)" > /dev/null 2>&1
 
 	# Record the number of non-NULL values that are produced.
 	n_dimension_non_null=$(psql $database -t -c "SELECT COUNT(*) FROM library WHERE ${dimension}_scrubbed IS NOT NULL")
