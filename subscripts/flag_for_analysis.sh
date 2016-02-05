0e3c7e0ac45de2e2595aba4ba1ad6d9e22cbc09d
dnorum <don@donnorum.com>
2016-02-05 15:53:42 -0500

    Adds flag.

--- subscripts/flag_for_analysis.sh
+++ subscripts/flag_for_analysis.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
-###################
-### DEFINITIONS ###
-###################
+# Run the query to flag books for inclusion in cluster analysis.
+psql $database -c "$(sed ${DIR}/sql/select_for_cluster_analysis.sql)" > /dev/null 2>&1
 
+# Record the number of books selected and omitted.
+n_selected=$(psql $database -t -c "SELECT COUNT(*) FROM library WHERE include_cluster_analysis")
+n_omitted=$(psql $database -t -c "SELECT COUNT(*) FROM library WHERE NOT include_cluster_analysis")
 
+# Trim whitespace.
+n_selected=${n_selected// /}
+n_omitted=${n_omitted// /}
+
+# Status update.
+echo "$n_selected books selected for cluster analysis; $n_omitted books omitted from analysis."
