b8a2b548bc0b81fc113d7f5555194b1477d2394d
Don Norum <don@donnorum.com>
2016-02-04 23:27:08 -0500

    Adds file to copy / convert resources.

---
+++ subscripts/setup_resources.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+###########################################################
+##
+###########################################################
+
+python ${DIR}/python/tsv_to_csv.py < ${DIR}/resources/librarything.tsv > ${DIR}/resources/librarything.csv
