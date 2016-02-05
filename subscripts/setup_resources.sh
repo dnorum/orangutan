8849cfab55b3b886c1edfa0f2bdeaee8e8e35e04
dnorum <don@donnorum.com>
2016-02-05 08:46:04 -0500

    Adds comment and renames python file.

--- subscripts/setup_resources.sh
+++ subscripts/setup_resources.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-###########################################################
-##
-###########################################################
-
-python ${DIR}/python/tsv_to_csv.py < ${DIR}/resources/librarything.tsv > ${DIR}/resources/librarything.csv
+# Convert the TSV dump file from LibraryThing into CSV using a Python script.
+python ${DIR}/python/tsv2csv.py < ${DIR}/resources/librarything.tsv > ${DIR}/resources/librarything.csv
