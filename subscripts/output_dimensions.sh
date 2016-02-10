c7857937ba8ae1c243f68402410fb2292b15e211
Don Norum <don@donnorum.com>
2016-02-10 08:13:20 -0500

    Removes collection criterion from selection condition.

--- subscripts/output_dimensions.sh
+++ subscripts/output_dimensions.sh
@@ -13,7 +13,7 @@ echo
 
 # Output raw data with book dimensions and record the number of rows exported
 # for the summary plots.
-n_records_dumped_summary=$(psql $database -c "$(sed -e "s@\${DIR}@${DIR}@g" -e "s/\${max_height}/${max_height}/g" -e "s/\${min_height}/${min_height}/g" -e "s/\${max_width}/${max_width}/g" -e "s/\${min_width}/${min_width}/g" -e "s/\${n_intervals}/${n_intervals}/g" ${DIR}/sql/output_dimensions_for_analysis.sql)")
+n_records_dumped_summary=$(psql $database -c "$(sed -e "s@\${DIR}@${DIR}@g" -e "s/\${max_height}/${max_height}/g" -e "s/\${min_height}/${min_height}/g" -e "s/\${max_width}/${max_width}/g" -e "s/\${min_width}/${min_width}/g" -e "s/\${n_intervals}/${n_intervals}/g" ${DIR}/sql/output_dimensions_for_summary.sql)")
 
 # Scrub the output of the postgres command to just the number of rows dumped.
 n_records_dumped_summary=${n_records_dumped_summary//[a-zA-Z ]/}
