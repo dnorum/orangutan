e710f1c87a74993ed085dffb9ffade96ab0b1be5
dnorum <don@donnorum.com>
2016-02-12 15:14:11 -0500

    Fixes file paths.

--- subscripts/load_clusters.sh
+++ subscripts/load_clusters.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Create the table in the database to hold the cluster file's data.
-psql $database -f ${DIR}/subscripts/sql/create_cluster_table.sql > /dev/null 2>&1
+psql $database -f ${DIR}/sql/create_cluster_table.sql > /dev/null 2>&1
 
 # Status update.
 echo "public.library_clusters created in $database."
 
 # Load the cluster analysis output file and record the number of rows imported.
-n_records_loaded=$(psql $database -f ${DIR}/subscripts/sql/load_cluster_table.sql)
+n_records_loaded=$(psql $database -f ${DIR}/sql/load_cluster_table.sql)
 
 # Scrub the output of the postgres command to just the number of rows loaded.
 n_records_loaded=${n_records_loaded//[a-zA-Z ]/}
