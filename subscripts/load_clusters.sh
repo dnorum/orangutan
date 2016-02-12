84d90dea0072e83443164ee2e8d457c03214c519
dnorum <don@donnorum.com>
2016-02-12 13:23:35 -0500

    Cleans up comments.

--- subscripts/load_clusters.sh
+++ subscripts/load_clusters.sh
@@ -6,13 +6,12 @@ psql $database -f ${DIR}/subscripts/sql/create_cluster_table.sql > /dev/null 2>&
 # Status update.
 echo "public.library_clusters created in $database."
 
-# Load the LibraryThing dump file (converted to CSV) into the database and
-# record the number of rows imported.
+# Load the cluster analysis output file and record the number of rows imported.
 n_records_loaded=$(psql $database -f ${DIR}/subscripts/sql/load_cluster_table.sql)
 
 # Scrub the output of the postgres command to just the number of rows loaded.
 n_records_loaded=${n_records_loaded//[a-zA-Z ]/}
 
 # Status update.
-echo "${n_records_loaded} records from cluster analysis dump file loaded into public.library_clusters."
+echo "${n_records_loaded} records from cluster analysis file loaded into public.library_clusters."
 echo
