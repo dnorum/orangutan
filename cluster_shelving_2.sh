2583d468415e414a8d6e8c26a453fd36acf2d14d
dnorum <don@donnorum.com>
2016-02-12 13:25:45 -0500

    Adds to-do comment stubs.

--- cluster_shelving_2.sh
+++ cluster_shelving_2.sh
@@ -6,18 +6,6 @@ DIR=$(dirname "$(readlink -f "$0")")
 # Define the name of the database to be used.
 database="cluster_analysis"
 
-# Define the minimum and maximum values considered plausible for each of the
-# physical dimensions. (Inches and pounds.) Note that these may need to be
-# adjusted as the script is run iteratively.
-height_min=4
-height_max=12 # N/A
-width_min=3
-width_max=10 # N/A
-thickness_min=0.0
-thickness_max=12 # N/A
-weight_min=0.0
-weight_max=25
-
 # Define the settings for the summary histograms.
 n_intervals=100
 
@@ -26,3 +14,18 @@ echo
 
 # Create the table public.library_clusters and load in the R cluster file.
 source ./subscripts/load_clusters.sh
+
+
+
+
+
+# Add the clusters to the main library table.
+
+# Summarize each cluster - the same statistics as before, but including the
+# total thickness (shelf-length) of the cluster.
+
+# Output the dimensional data for each cluster.
+
+# Plot the histograms and heat maps for each cluster.
+
+# Clean things up and zip output folders for convenience.
