afd1cf39c42a6c64b34d418d6eaa6b0aaef9abff
dnorum <don@donnorum.com>
2016-02-12 15:15:15 -0500

    Fixes file path.

--- sql/load_cluster_table.sql
+++ sql/load_cluster_table.sql
@@ -1,6 +1,6 @@
 -- Load the R cluster analysis CSV dump into the table created above. Note that
 -- it's currently given as an absolute path.
-COPY library_clusters FROM '/home/don/projects/git/cluster_shelving/r/kmeans_out.csv' 
+COPY library_clusters FROM '/home/don/projects/git/cluster_analysis/r/kmeans_out.csv' 
 	DELIMITER ','
 	NULL ''
 	CSV
