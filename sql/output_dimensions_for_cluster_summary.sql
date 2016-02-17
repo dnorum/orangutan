555847717f5e0b06339d7ba78ef423c3a51715e5
dnorum <don@donnorum.com>
2016-02-17 15:07:46 -0500

    Outputs thickness-weighted histogram data.

--- sql/output_dimensions_for_cluster_summary.sql
+++ sql/output_dimensions_for_cluster_summary.sql
@@ -20,6 +20,27 @@ TO
 
 COPY
 	(	SELECT
+			((${max_height}-${min_height})::DOUBLE PRECISION/${n_intervals}::DOUBLE PRECISION) * floor(height_scrubbed::DOUBLE PRECISION/((${max_height}-${min_height})::DOUBLE PRECISION/${n_intervals}::DOUBLE PRECISION)) + ((${max_height}-${min_height})::DOUBLE PRECISION/${n_intervals}::DOUBLE PRECISION)/2.0  AS "Height Bin"
+		,	((${max_width}-${min_width})::DOUBLE PRECISION/${n_intervals}::DOUBLE PRECISION) * floor(width_scrubbed::DOUBLE PRECISION/((${max_width}-${min_width})::DOUBLE PRECISION/${n_intervals}::DOUBLE PRECISION)) + ((${max_width}-${min_width})::DOUBLE PRECISION/${n_intervals}::DOUBLE PRECISION)/2.0  AS "width (Width) Bin"
+		,	SUM(thickness_scrubbed) AS "Inches of Books"
+		FROM
+			library
+		WHERE
+			"cluster" = ${cluster}::TEXT
+		AND	thickness_scrubbed IS NOT NULL
+		GROUP BY
+			1, 2
+		ORDER BY
+			1 ASC, 2 ASC)
+TO
+	'${DIR}/working/cluster_${cluster}/book_heights_widths_summary.csv'
+(	FORMAT csv
+,	DELIMITER ','
+,	HEADER
+,	NULL ''	);
+
+COPY
+	(	SELECT
 			book_id AS "Book ID"
 		,	height_scrubbed AS "Height (in)"
 		,	width_scrubbed AS "Width (in)"
--- sql/output_dimensions_for_summary.sql
+++ sql/output_dimensions_for_summary.sql
@@ -21,6 +21,28 @@ TO
 
 COPY
 	(	SELECT
+			((${max_height}-${min_height})/${n_intervals}) * floor(height_scrubbed/((${max_height}-${min_height})/${n_intervals})) + ((${max_height}-${min_height})/${n_intervals})/2.0  AS "Height Bin"
+		,	((${max_width}-${min_width})/${n_intervals}) * floor(width_scrubbed/((${max_width}-${min_width})/${n_intervals})) + ((${max_width}-${min_width})/${n_intervals})/2.0  AS "width (Width) Bin"
+		,	SUM(thickness_scrubbed) AS "Inches of Books"
+		FROM
+			library
+		WHERE
+			height_scrubbed IS NOT NULL
+		AND	width_scrubbed IS NOT NULL
+		AND	thickness_scrubbed IS NOT NULL
+		GROUP BY
+			1, 2
+		ORDER BY
+			1 ASC, 2 ASC	)
+TO
+	'${DIR}/working/book_heights_widths_weighted_summary.csv'
+(	FORMAT csv
+,	DELIMITER ','
+,	HEADER
+,	NULL ''	);
+
+COPY
+	(	SELECT
 			book_id AS "Book ID"
 		,	height_scrubbed AS "Height (in)"
 		,	width_scrubbed AS "Width (in)"
