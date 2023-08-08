22c1cff2949f314288e09249d190f57da0299dc8
dnorum <dnorum>
2023-08-08 16:51:31 -0400

    Adds loading of configuration settings (postgresql credentials) from JSON file.

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
+import json
+import psycopg
 import sys
-sys.path.append("python/packages/tsv2csv")
 
+# Set up manual importing of under-development packages from within the repo.
+sys.path.append("python/packages/tsv2csv")
+# TODO: At some point, publish this - or find a previously-published replacement.
 import tsv2csv
 
+# TODO: Clean up how credentials are handled.
+with open("config/credentials.json") as jsonFile:
+    credentials = json.load(jsonFile)
+
+dbConnection = psycopg.connect( dbname=credentials['postgres']['bootstrapDbName']
+                            ,   user=credentials['postgres']['user']
+                            ,   host=credentials['postgres']['host']
+                            ,   password=credentials['postgres']['password'] )
+
 # Convert the TSV dump file from LibraryThing into CSV.
-tsv2csv.tsv2csv("sample_data/librarything_sample.tsv")
+#tsv2csv.tsv2csv("sample_data/librarything_sample.tsv")
