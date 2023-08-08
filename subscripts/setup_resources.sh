0762690382306cebfb748650f705091600247773
dnorum <dnorum>
2023-08-08 11:46:28 -0400

    Removes deprecated shell script.

--- subscripts/setup_resources.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Convert the TSV dump file from LibraryThing into CSV using a Python script.
-python ${DIR}/python/tsv2csv.py < ${DIR}/resources/librarything.tsv > ${DIR}/resources/librarything.csv
-
-# Copy the kmeans.R script from /r_scripts into the /r working directory.
-cp ${DIR}/r_scripts/kmeans.R ${DIR}/r/kmeans.R
