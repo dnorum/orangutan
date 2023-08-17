e326db0ceb0e7fc91a69271b314192655c6ac396
dnorum <dnorum>
2023-08-17 10:19:09 -0400

    Refactors handling of postgres credentials and configuration and adds sample/ folder.

--- main.py
+++ main.py
@@ -5,25 +5,25 @@ import sys
 # Set up manual importing of under-development packages from within the repo.
 sys.path.append("python/packages/database")
 sys.path.append("python/packages/libraryThingDatabase")
-sys.path.append("python/packages/tsv2csv")
 # TODO: At some point, publish this - or find a previously-published replacement.
+sys.path.append("python/packages/tsv2csv")
 import database
 import libraryThingDatabase
 import tsv2csv
 
-# TODO: Clean up how credentials are handled.
-with open("config/credentials.json") as jsonFile:
-    credentials = json.load(jsonFile)
+with open("config/postgres.json") as jsonFile:
+    postgresConfig = json.load(jsonFile)
+with open("credentials/postgres.json") as jsonFile:
+    postgresCredentials = json.load(jsonFile)
 
-with psycopg.connect(   dbname=credentials['postgres']['bootstrapDbName']
-                    ,   user=credentials['postgres']['user']
-                    ,   host=credentials['postgres']['host']
-                    ,   password=credentials['postgres']['password'] ) as connection:
+connectionSettings = {**postgresConfig["bootstrap"], **postgresCredentials["bootstrap"]}
+
+with psycopg.connect(**connectionSettings) as connection:
     connection.autocommit = True
     with connection.cursor() as cursor:
         # TODO: Find a workaround to avoid injection, but apparently PostgreSQL
-        # allow for parameterization of CREATE DATABASE...
-        dbName = credentials['postgres']['dbName']
+        # doesn't allow for parameterization of CREATE DATABASE...
+        dbName = postgresConfig["prod"]["dbname"]
         if not database.databaseExists(connection, dbName):
             query = "CREATE DATABASE " + dbName + ";"
             cursor.execute(query)
