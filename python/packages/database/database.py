4645ff8de437a1a2ff5dd2923227ece07df39d73
dnorum <dnorum>
2023-08-17 17:33:53 -0400

    Turns identifier-format check into standalone function.

--- python/packages/database/database.py
+++ python/packages/database/database.py
@@ -12,22 +12,26 @@ def databaseExists(connection, dbName):
         print(error)
     return exists
 
+def validateIdentifier(identifier):
+    # Note: This is close to, but not exactly what's defined in the PostgreSQL standard -
+    # since the goal is to belt-and-suspenders against injection (unintentional or otherwise),
+    # it clamps down on allowable characters, but does _not_ check for reserved keywords.
+    formatString = '[A-Za-z_][A-Za-z0-9_]{,62}'
+    format = re.compile(formatString)
+    if not re.fullmatch(format, identifier):
+        raise ValueError('Identifier: "' + identifier + '" violates format requirement: /' + formatString + '/')
+    return 0
+
 def createDatabase(connectionSettings, dbName):
     with psycopg.connect(**connectionSettings) as connection:
         connection.autocommit = True
         with connection.cursor() as cursor:
             if not databaseExists(connection, dbName):
-                query = "CREATE DATABASE " + dbName + ";"
                 # PostgreSQL doesn't allow for parameterization of CREATE DATABASE, so check the database name
                 # manually.
-                # Note: This is close to, but not exactly what's defined in the PostgreSQL standard -
-                # since the goal is to belt-and-suspenders against injection (unintentional or otherwise),
-                # it clamps down on allowable characters, but does _not_ check for reserved keywords.
-                # TODO: Separate into a separate validateIdentifier(string) function.
-                formatString = '[A-Za-z_][A-Za-z0-9_]{,62}'
-                check = re.compile('CREATE DATABASE ' + formatString + '\;')
-                if not re.fullmatch(check, query):
-                    raise ValueError('Database Name: "' + dbName + '" violates format requirement: /' + formatString + '/')
+                validateIdentifier(dbName)
+                query = "CREATE DATABASE " + dbName + ";"
+                # QUESTION: Additional check here, maybe?
                 cursor.execute(query)
             else:
                 print("Database {dbName} already exists.".format(**{'dbName': dbName}))
