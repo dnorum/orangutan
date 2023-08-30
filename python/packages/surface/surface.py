84e7e0f36d406ff51bebe573ada6b6e9d51941bc
dnorum <dnorum>
2023-08-30 09:32:44 -0400

    Pins extra functionality for later.

--- python/packages/surface/surface.py
+++ python/packages/surface/surface.py
@@ -6,6 +6,8 @@ height, and total thickness, respectively).
 
 class Range:
     """A minimum and a maximum value for a value or dimension."""
-    def __init__(self, min, max, inclusive=True, continuous=False, interval=0):
+    # TODO: Add ", inclusive=True, continuous=False, interval=0"
+    def __init__(self, min, max):
         self.min = min
         self.max = max
+
