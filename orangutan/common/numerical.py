d3a32d84b58c21c9f2dd1ae51e4fea4b667a464c
dnorum <dnorum>
2023-09-08 16:40:03 -0400

    Adds dosctring to module and comments to decimals(); fixes ValueError to TypeError; adds fallback error at end of function.

--- orangutan/common/numerical.py
+++ orangutan/common/numerical.py
@@ -1,3 +1,20 @@
+"""Oddball, niche, and/or commonly reused (here, at least) numerical functions.
+
+A collection of functions that deal with numbers or numerical data and:
+    - Are used in multiple subpackages, or
+    - _Might_ realistically be used in multiple subpackages; and
+    
+    - Are longer than a single, straightforward line of code, or
+    - Require some sort of specific error-handling or checking, or
+    - Do weird, un-numberly things to numbers, like converting a float into a
+        string and then counting digits backwards to figure out how many decimal
+        places it has.
+
+Typical usage examples:
+
+    grid_precision = decimals(range.interval)
+"""
+
 def decimals(number):
     """Given a number, return the number of decimal places it has.
     
@@ -11,10 +28,14 @@ def decimals(number):
     if isinstance(number, int):
         return 0
     if not isinstance(number, float):
-        raise ValueError(f"Number ({number}) must be integer or float, not "
+        # docstring API specifies integer or float, but... goofers gonna goof.
+        raise TypeError(f"Number ({number}) must be integer or float, not "
                          f"{type(number)}.")
     if isinstance(number, float):
         return str(number)[::-1].find('.')
+    # No idea how you might get here, but just in case.
+    raise RuntimeError(f"{__name__}.decimals: number seems to be neither an "
+                       "int nor a float, AND not-NOT a float.")
 # 1
 # 2
 # 3
